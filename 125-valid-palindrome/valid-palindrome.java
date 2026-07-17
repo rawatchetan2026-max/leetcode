8b385caffb5cb5d040dca668a1c5ea8981a040d2
Chetan Rawat <rawatchetan2026@gmail.com>
2026-07-17 20:53:20 +0530

    Time: 24 ms (10.02%) | Memory: 47.7 MB (10.77%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -6,12 +6,25 @@ class Solution {
     //     }
 
     //    return s2.equals(s1);
-    String cleanStr = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+    // String cleanStr = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
         
-        // Use StringBuilder to reverse efficiently
-        StringBuilder sb = new StringBuilder(cleanStr);
-        String reversedStr = sb.reverse().toString();
+    //     // Use StringBuilder to reverse efficiently
+    //     StringBuilder sb = new StringBuilder(cleanStr);
+    //     String reversedStr = sb.reverse().toString();
         
-        return cleanStr.equals(reversedStr);
+    //     return cleanStr.equals(reversedStr);
+            String s1 = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        String s2 = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int res = 0;
+        for (int i = s2.length() - 1; i >= s2.length() / 2; i--) {
+            if (s1.charAt(i) == s2.charAt(s2.length() - 1 - i)) {
+                res = 1;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
